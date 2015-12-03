25d5895bbc92e677737696e70d5a8ff4d34703dd
Rosanna Fong <me@rosanna.co.nz>
2015-12-03 16:46:36 +1300

    Reverted changes.

--- js/body.js
+++ js/body.js
@@ -1,3 +1,5 @@
+//@prepros-append vendor/jquery.js
+//@prepros-append foundation.min.js
 //@prepros-append templates/primary-nav.js
 //@prepros-append templates/footer.js
 //@prepros-append templates.js
